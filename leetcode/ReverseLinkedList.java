bcea1f240bb9ed02431e52433044a86c7ee2e0a0
Siddhi Gupta <siddhi.04g@gmail.com>
2022-12-19 19:15:48 +0530

    Ques-206

---
+++ leetcode/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+//Ques-206
+import LinkedList.LL.*;
+public class ReverseLinkedList {
+    public Node reverseList(Node head) {
+        Node prev=null;
+        Node curr=head;
+        Node temp;
+        while(curr!=null){
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+     }
+}
