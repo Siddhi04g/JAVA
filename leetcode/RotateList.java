241f6e773d39e88a185c4ff43a5037311ee93124
Siddhi Gupta <siddhi.04g@gmail.com>
2022-12-19 19:14:26 +0530

    Ques-61

---
+++ leetcode/RotateList.java
@@ -0,0 +1,22 @@
+//Ques-61
+
+import LinkedList.LL.*;
+public class RotateList {
+    public Node oddEvenList(Node head) {
+    if (head == null) 
+            return null;
+        Node odd = head, even = head.next, evenHead = even;
+        
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenHead;
+        even=null;
+        return head;
+    }
+}
+
