81c5897d27b9f0d213d92a73f2b7890a40e28f31
Siddhi Gupta <siddhi.04g@gmail.com>
2022-12-19 19:12:48 +0530

    Ques -21

---
+++ leetcode/MergeTwoSortedList.java
@@ -0,0 +1,35 @@
+//Ques- 21
+
+import LinkedList.LL.*;
+public class MergeTwoSortedList {
+    public Node mergeTwoLists(Node list1,Node list2) {
+        Node f=list1;
+        Node s=list2;
+        // LinkedList<Integer> ans = new LinkedList<Integer>();
+        Node ans=new Node(-1);
+        Node temp=ans;
+        while(f!=null && s!=null){
+            if(f.val<=s.val){
+                ans.next=f;
+                ans=ans.next;
+                f=f.next;
+            }else{
+                ans.next=s;
+                ans=ans.next;
+                s=s.next;
+            }
+        }
+        while(f!=null){
+            ans.next=f;
+            ans=ans.next;
+            f=f.next;
+        }
+        while(s!=null){
+            ans.next=s;
+            ans=ans.next;
+            s=s.next;
+        }
+        return temp.next;
+    }
+}
+
