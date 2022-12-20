ee22eeccb4b2b7e33e28ce90d9d378e76f8c393c
Siddhi Gupta <siddhi.04g@gmail.com>
2022-12-20 11:28:36 +0530

    Ques-83

---
+++ leetcode/PartitionList.java
@@ -0,0 +1,28 @@
+//Ques-83
+import LinkedList.LL.*;
+public class PartitionList {
+    public Node partition(Node head, int x) {
+        Node list=new Node(-1);
+        Node ans=list;
+        Node node=head;
+        while(node!=null){
+            if(node.val<x){
+                Node dummy=new Node(node.val);
+                list.next=dummy;
+                list=list.next;
+            }
+            node=node.next;
+        }
+        node=head;
+        while(node!=null){
+            if(node.val>=x){
+                Node dummy=new Node(node.val);
+                list.next=dummy;
+                list=list.next;   
+            }
+            node=node.next;
+        }
+        return ans.next;
+    }
+    
+}
