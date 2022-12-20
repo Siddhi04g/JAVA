97fa77896b7ac46de046c086f78a6ed5d4f9453d
Siddhi Gupta <siddhi.04g@gmail.com>
2022-12-20 12:23:37 +0530

    Updated

--- LinkedList/leetcode/PartitionList.java
+++ LinkedList/leetcode/PartitionList.java
@@ -1,4 +1,17 @@
 //Ques-83
+
+/* Approach
+Question asks you to put nodes whose value is smaller than x first and then the ones whose value is larger or equal to x
+So, just create a newList 'list'
+Now just traverse the whole Linked List and if my value of node is less than the x the add that node's value to that new List
+same thing do for adding nodes whose value larger or equal to x by traversing the whole linkedlist again and adding the required node's value to the new list.
+Now you r good to go!!!
+
+Complexity
+Time complexity: O(n)
+
+Space complexity: O(n) 
+*/
 package LinkedList.leetcode;
 import LinkedList.LL.*;
 public class PartitionList {
