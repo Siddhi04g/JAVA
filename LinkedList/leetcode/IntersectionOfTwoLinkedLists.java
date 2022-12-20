e4c74a5c23618ddefe012325f4d26779987d3e39
Siddhi Gupta <siddhi.04g@gmail.com>
2022-12-20 12:12:08 +0530

    Linked List

---
+++ LinkedList/leetcode/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,33 @@
+//Ques-160
+package LinkedList.leetcode;
+import LinkedList.LL.*;
+public class IntersectionOfTwoLinkedLists {
+    public Node getIntersectionNode(Node headA, Node headB) {
+        Node nodeA=headA;
+        Node nodeB=headB;
+        int countA=0,countB=0;
+        while(nodeA!=null){
+            countA++;
+            nodeA=nodeA.next;
+        }
+        while(nodeB!=null){
+            countB++;
+            nodeB=nodeB.next;
+        }
+        nodeA=headA;
+        nodeB=headB;
+        while(countA>countB){
+            nodeA=nodeA.next;
+            countA--;
+        }
+        while(countB>countA){
+            nodeB=nodeB.next;
+            countB--;
+        }
+        while(nodeA!=nodeB){
+            nodeA=nodeA.next;
+            nodeB=nodeB.next;
+        }
+        return nodeA;
+    }
+}
